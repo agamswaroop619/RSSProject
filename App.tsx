a5df0fb7295c4cf5595e5a737e9406ac97d9b3da
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-27 22:15:56 +0530

    add: New landing page check

--- App.tsx
+++ App.tsx
@@ -71,24 +71,11 @@ function App(): JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> Hello
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+             <Text style={styles.innerText}> Hello World</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -106,11 +93,21 @@ const styles = StyleSheet.create({
   },
   sectionDescription: {
     marginTop: 8,
-    fontSize: 18,
+    display: 'flex',
+    alignContent: 'center',
+    fontSize: 58,
+    height:'100%',
+    width:'100%',
     fontWeight: '400',
+    color : 'white',
+    justifyContent : 'center',
+    backgroundColor:'blue',
   },
   highlight: {
     fontWeight: '700',
+  },
+  innerText: {
+    color: 'white',
   },
 });
 
